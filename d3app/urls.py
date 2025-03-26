9f9318df1e1a31dd3bbc5056aadb6fcf97d0fa0d
Le Thuy Quynh <122433706+lequynhlt@users.noreply.github.com>
2025-03-26 19:27:54 +0700

    Update urls.py

--- d3app/urls.py
+++ d3app/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.data_visualization, name='data_visualization'),  # Trang chính hiển thị biểu đồ
-    path('import/', views.import_csv, name='import_csv'),  # Trang nhập dữ liệu CSV
-]
+    path('', views.data_visualization, name='data_visualization'), 
+    path('import/', views.import_csv, name='import_csv'), 
